db4827454f8bb0d64638935777f581c7da41c2ff
Shubh Purwar <60182258+striver01@users.noreply.github.com>
2021-05-22 04:00:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function myfun(){
+    console.log(3);
+    var ham = document.getElementById('ham');
+    var showHide = document.getElementById('bar');
+    var btn = document.getElementById('sidebar');
+    var show = document.getElementById('cross');
+    if(showHide.style.display !== "block" && showHide.style.display !== "block"){
+        $('#bar').addClass('animated bounceIn');
+        showHide.style.display = "block";
+        $('#sidebar').addClass('animated backInRight');
+        btn.style.display = "block";
+        $('#cross').addClass('animated bounceInDown');
+        show.style.display = "block";
+    }
+}
+function myfun1(){
+    console.log(4);
+    var ham = document.getElementById('ham');
+    var showHide = document.getElementById('bar');
+    var btn = document.getElementById('sidebar');
+    var show = document.getElementById('cross');
+    showHide.style.display = "none";
+    btn.style.display = "none";
+    show.style.display = "none";
+}
